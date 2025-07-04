c11ffe3712f8244f2d016719bade39fa97411655
Yegor <Ollie_0@mail.ru>
2025-07-04 10:03:16 +0700

    added templates, render and path

--- core/views.py
+++ core/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Барбершоп')
+    return render(request, 'index.html')
 
 def thanks(request):
-    return HttpResponse('Ваша заявка принята!')
+    return render(request, 'thanks.html')
 
 def orders(request):
-    return HttpResponse('Список заявок')
+    return render(request, 'orders.html')
 
 def order_detail(request, order_id):
-    return HttpResponse(f'Заявка № {order_id}')
+    return render(request, 'order_detail.html',  {"order_id": order_id})
