9fa4ab271133b371dcf7dfdc8f96dcc2230a72d7
Yegor <Ollie_0@mail.ru>
2025-07-11 17:24:03 +0700

    hw_39: added paths for new pages

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('thanks/', views.thanks, name='thanks'),
     path('orders/', views.orders, name='orders'),
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('masters/', views.masters_view, name='masters'),
+    path('services/', views.services_view, name='services'),
+    path('appointment/', views.appointment, name='appointment'),
 ]
