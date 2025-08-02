47bc8f8739110271b03a13bd29dfc2effe2638f8
Ollie <Ollie_0@mail.ru>
2025-08-02 13:19:35 +0700

    hw_41: added PATH of media to URLs (for DEBUG=True)

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('services/', views.services_view, name='services'),
     path('appointment/', views.appointment, name='appointment'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
