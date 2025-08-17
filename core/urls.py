168d703029f785017da0e9f40e7625f2b0013456
Ollie <Ollie_0@mail.ru>
2025-08-17 11:35:38 +0700

    hw_46: update all URL routes to use CBV .as_view() methods

--- core/urls.py
+++ core/urls.py
@@ -4,15 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('thanks/', views.thanks, name='thanks'),
-    path('orders/', views.orders, name='orders'),
-    path('orders/<int:pk>/', views.order_detail, name='order_detail'),
-    path('masters/', views.masters_view, name='masters'),
-    path('services/', views.services_view, name='services'),
-    path('appointment/', views.appointment, name='appointment'),
-    path('review/create/', views.create_review, name='create_review'),
-    path('orders/create/', views.create_order, name='create_order'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('thanks/', views.ThanksView.as_view(), name='thanks'),
+    path('orders/', views.OrdersListView.as_view(), name='orders'),
+    path('orders/<int:pk>/', views.OrderDetailView.as_view(), name='order_detail'),
+    path('masters/', views.MastersView.as_view(), name='masters'),
+    path('services/', views.ServicesView.as_view(), name='services'),
+    path('appointment/', views.AppointmentView.as_view(), name='appointment'),
+    path('review/create/', views.ReviewCreateView.as_view(), name='create_review'),
+    path('orders/create/', views.OrderCreateView.as_view(), name='create_order'),
     path('ajax/get_master_services/', __import__('core.ajax').ajax.get_master_services, name='get_master_services'),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
