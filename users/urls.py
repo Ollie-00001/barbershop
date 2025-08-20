6d02d95d6ab9881c760dcdff52f12ff1e3033ee2
Ollie <Ollie_0@mail.ru>
2025-08-20 15:19:41 +0700

    hw_48: add all user management and password reset routes

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
-from .views import UserLoginView, UserLogoutView, UserRegistrationView
+from .views import (
+    UserRegisterView, UserLoginView, UserLogoutView,
+    UserProfileDetailView, UserProfileUpdateView,
+    UserPasswordChangeView,
+    CustomPasswordResetView, CustomPasswordResetDoneView,
+    CustomPasswordResetConfirmView, CustomPasswordResetCompleteView
+)
 
 urlpatterns = [
+    path('register/', UserRegisterView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('profile/<int:pk>/', UserProfileDetailView.as_view(), name='profile_detail'),
+    path('profile/edit/', UserProfileUpdateView.as_view(), name='profile_edit'),
+    path('password_change/', UserPasswordChangeView.as_view(), name='password_change'),
+    path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
